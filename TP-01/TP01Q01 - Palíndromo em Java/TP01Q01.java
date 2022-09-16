26de8ef2256655b52f86bbaa817938a3843cd105
Pedro Lopes <lopescostapedrohenrique@gmail.com>
2022-09-16 11:13:19 -0300

    Update Tab Size

--- TP-01/TP01Q01 - Palíndromo em Java/TP01Q01.java
+++ TP-01/TP01Q01 - Palíndromo em Java/TP01Q01.java
@@ -1,45 +1,46 @@
 import java.util.Scanner;
 
 class TP01Q1 {
-  public static void main(String[] args) {
 
-    Scanner scanner = new Scanner (System.in);
-    boolean[] check = new boolean[1000];
-    
-    int a = 0;
-    int i = 0;
-    
-    do {
-      String str1 = "", str2 = ""; //str1 = digitada / str2 = reversa
-      
-      str1 = scanner.nextLine(); //lê a palavra/frase digitada
+	public static void main(String[] args) {
 
-      if (str1.equals("FIM")) {
-        a = 1;
-      }
-      
-      int strLength = str1.length();
+		Scanner scanner = new Scanner (System.in);
+		boolean[] check = new boolean[1000];
+		
+		int a = 0;
+		int i = 0;
+		
+		do {
+		String str1 = "", str2 = ""; //str1 = digitada / str2 = reversa
+		
+		str1 = scanner.nextLine(); //lê a palavra/frase digitada
 
-      for (int y = (strLength - 1); y>=0; --y) {
-        str2 = str2 + str1.charAt(y);
-      } //coloca a palavra/frase reversa na str2
-    
+		if (str1.equals("FIM")) {
+			a = 1;
+		}
+		
+		int strLength = str1.length();
 
-      if (str1.equals(str2) && a != 1) {   //compara
-        check[i] = true;
-        i++;
-      } else if (a != 1) {
-        check[i] = false;
-        i++;
-      }
-    } while (a != 1);
+		for (int y = (strLength - 1); y>=0; --y) {
+			str2 = str2 + str1.charAt(y);
+		} //coloca a palavra/frase reversa na str2
+		
 
-    for (int x = 0; x < i; x++) {
-      if (check[x] == true) {
-        System.out.println("SIM");
-      } else {
-        System.out.println("NÃO");
-      }
-    }
-  }
+		if (str1.equals(str2) && a != 1) {   //compara
+			check[i] = true;
+			i++;
+		} else if (a != 1) {
+			check[i] = false;
+			i++;
+		}
+		} while (a != 1);
+
+		for (int x = 0; x < i; x++) {
+		if (check[x] == true) {
+			System.out.println("SIM");
+		} else {
+			System.out.println("NÃO");
+		}
+		}
+	}
 }
