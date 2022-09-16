dedec11597f65ee82ac5610b0d92365ca5dec7e0
Pedro Lopes <lopescostapedrohenrique@gmail.com>
2022-09-16 16:46:00 -0300

    Update TP01Q03.java

--- TP-01/TP01Q03 - Ciframento em Java/TP01Q03.java
+++ TP-01/TP01Q03 - Ciframento em Java/TP01Q03.java
@@ -4,8 +4,8 @@ class TP01Q03
       
     public static boolean isFim(String s) { return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M'); }
 
-    public static String codCesar(String origString)
-    {
+    public static String codCesar(String origString) {
+        
         char[] chars = origString.toCharArray();
 
         for(int x = 0; x != origString.length(); x++) chars[x] += 3;
@@ -14,8 +14,8 @@ class TP01Q03
 
     // --------------------------------------------------------------------------------------------------------------- //
      
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+
         String[] linha = new String[1000];
 
 		int numLinhas = 0;
